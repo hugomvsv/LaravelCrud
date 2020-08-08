5f4681b0995967b839a0b3d4a5fbceceeedfe5d6
hugomvsv <evieira1998@gmail.com>
2020-08-08 18:43:32 +0100

    Add migration and Seeder

---
+++ database/factories/UserFactory.php
@@ -0,0 +1,15 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\User;
+use Faker\Generator as Faker;
+use Illuminate\Support\Str;
+
+$factory->define(User::class, function (Faker $faker) {
+    return [
+        'name' => $faker->name,
+        'email' => $faker->unique()->safeEmail,
+       'birth'=>$faker->dateTime(),
+    ];
+});
